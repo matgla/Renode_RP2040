d31a9af89175adf6c4d15db1f5714c9a55edef19
Mateusz Stadnik <matgla@live.com>
2024-11-27 13:28:38 +0100

    pico-sdk compatible with examples

--- fetch_pico_examples.bat
+++ fetch_pico_examples.bat
@@ -5,10 +5,6 @@ if not exist pico-examples (
     git clone https://github.com/raspberrypi/pico-examples.git
     cd pico-examples
     git checkout 7e77a0c381863be0c49086567e7f1934d78ac591
-    git clone https://github.com/raspberrypi/pico-sdk.git
-    cd pico-sdk 
-    git submodule update --init
-    cd ..
     cd ..
     for %%f in (pico_examples_patches/*.patch) do (
         cd pico-examples 
