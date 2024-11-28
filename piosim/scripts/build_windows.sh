c0827a214841875c6cb915d157032a4b25b704b5
Your Name <you@example.com>
2024-11-28 11:16:26 +0100

    added script to compile piosim automatically

--- piosim/scripts/build_windows.sh
+++ piosim/scripts/build_windows.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
-docker run --rm -v $SCRIPT_DIR:/workdir  -c "cd /workdir && ls -la && mkdir -p build_windows && cd build_windows && cmake .. -DCMAKE_BUILD_TYPE=Release && cmake --build ." 
+docker run --rm -v $SCRIPT_DIR:/workdir sh -c "cd /workdir && ls -la && mkdir -p build_windows && cd build_windows && cmake .. -DCMAKE_BUILD_TYPE=Release && cmake --build ." 
 	
