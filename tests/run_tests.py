1edab75b55a61ffd70c2032458ed14b5d115cc6f
Mateusz Stadnik <matgla@live.com>
2024-11-28 17:30:31 +0100

    added parallel execution and failures log storing

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,11 +7,25 @@ from argparse import ArgumentParser
 import psutil
 import sys
 import shutil
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing
+
+def run_test(command, test, retries):
+    print("Staring test:", test)
+    passed = False
+    for i in range(0, retries):
+        if run([command, test]).returncode == 0:
+            passed = True 
+            break
+    return passed, test
 
 parser = ArgumentParser()
 parser.add_argument("-f", "--file", help="Path to yaml file with tests")
 parser.add_argument("-r", "--retry", type=int, default=1, help="Number of retries of tests if failed")
 parser.add_argument("-e", "--renode_test", default="renode-test", help="renode-test executable path")
+parser.add_argument("-j", "--threads", default=multiprocessing.cpu_count(), help="Number of thread for renode tests")
+
+
 args, _ = parser.parse_known_args()
 
 script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
@@ -23,29 +37,26 @@ failed_tests=0
 passed_tests=0
 failed_names=[]
 
+
+tests_to_run = []
 with open(test_file, "r") as file:
     for line in file.readlines():
         if line.find("#") != -1:
             continue
 
         test_file = script_dir / line.removeprefix("- tests/").strip()
+        tests_to_run.append(str(test_file))
 
-        passed = False
-        for i in range(0, args.retry):
-            if run([str(runner), str(test_file)]).returncode == 0:
-                passed = True 
-                break
-            else:
-                for proc in psutil.process_iter():
-                    filtered_name = proc.name().lower()
-                    if filtered_name.find("renode") != -1:
-                        print("Killing potentialy orphaned renode process: " + str(proc.pid) + ", name: " + proc.name())
-                        proc.kill()
+with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+    futures = [executor.submit(run_test, str(runner), test, 3) for test in tests_to_run]
+
+    for future in as_completed(futures):
+        passed, test = future.result()
         if passed:
             passed_tests += 1
         else: 
             failed_tests += 1
-            failed_names.append(test_file)
+            failed_names += test
 
 print("Test passed: " + str(passed_tests) + "/" + str(failed_tests + passed_tests))
 print("Failed tests:")
